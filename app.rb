4e52161d070896be53619b073ef1d2951477962f
claudia v <claudia4ro@yahoo.com>
2020-06-16 01:10:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do 
     @num = params[:number]
     @phrase = params[:phrase]
-    "#{@num}.times { #{@phrase} }"
+    "4.times { #{@phrase} }"
     
     
   end 
